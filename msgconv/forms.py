bbdd1b17d233ba56254017185edef71119d9423a
Midori <martin.midori@gmail.com>
2024-07-27 16:26:00 +0200

    Added basic upload file upload functionality

--- msgconv/forms.py
+++ msgconv/forms.py
@@ -1,5 +1,4 @@
 from django import forms
 
 class MyFileUploadForm(forms.Form):
-    title = forms.CharField(max_length=100)
     file = forms.FileField()
--- msgconv/views.py
+++ msgconv/views.py
@@ -12,12 +12,12 @@ class LandingView(View):
 
     def post(self, request):
         form = MyFileUploadForm(request.POST, request.FILES)
+        print(request.__dict__)
         if form.is_valid():
             # Process the file here
-            title = form.cleaned_data['title']
             uploaded_file = request.FILES['file']
             file_content = uploaded_file.read()  # This reads the file into memory
             # You can then process the file_content as needed
-            
-            return render(request, 'index.html', {'title': title, 'file_name': uploaded_file.name})
+            print(file_content)
+            return render(request, 'index.html', {'file_name': uploaded_file.name})
         return render(request, 'index.html', {'form': form})
