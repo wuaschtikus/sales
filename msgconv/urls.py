18cd8f4303b00d8d949b95a14f8f86367174eaa4
Midori <martin.midori@gmail.com>
2024-08-03 12:01:54 +0200

    added html and fixed issue with index page linking

--- msgconv/urls.py
+++ msgconv/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import MsgConv, IndexView, ConverterView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path('index/', IndexView.as_view(), name='index'),
     path('converter/', ConverterView.as_view(), name='converter'),
     path('msgconv/', MsgConv.as_view(), name='msgconv'),
     path('upload/', MsgConv.as_view(), name='msgupload'),
