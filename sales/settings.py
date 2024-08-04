206d4d42f9d18eba7fa0c5f555ca01d3e000dcdd
Midori <martin.midori@gmail.com>
2024-08-04 14:53:51 +0200

    added database config for debug

--- sales/settings.py
+++ sales/settings.py
@@ -164,24 +164,24 @@ WSGI_APPLICATION = 'sales.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': DATABASE_NAME,
-        'USER': DATABASE_USER,
-        'PASSWORD': DATABASE_PASSSWORD,
-        'HOST': 'db',  # This matches the service name defined in docker-compose.yml
-        'PORT': '5432',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': DATABASE_NAME,
+            'USER': DATABASE_USER,
+            'PASSWORD': DATABASE_PASSSWORD,
+            'HOST': 'db',  # This matches the service name defined in docker-compose.yml
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
