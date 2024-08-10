1da7791cf0765826b6400e16633eb10901c94417
Midori <martin.midori@gmail.com>
2024-08-10 19:02:19 +0200

    added status

--- msgconv/views/index.py
+++ msgconv/views/index.py
@@ -1,5 +1,9 @@
+import logging
 from django.shortcuts import render
 from django.views import View
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 class IndexView(View):
     template_name = 'msgconv/index.html'
