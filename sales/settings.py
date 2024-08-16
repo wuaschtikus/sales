b40131b764fd7c323f5f96f8a2f37b480c272ca5
Midori <martin.midori@gmail.com>
2024-08-16 10:36:01 +0200

    tried to fix static files issue

--- sales/settings.py
+++ sales/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.staticfiles',
     'msgconv',
     'crispy_forms',
     'users',
