fcbda7e1d862da347f7f77ff0e5eeb7a9ec2265d
Midori <martin.midori@gmail.com>
2024-08-18 11:02:52 +0200

    added new migration for subscriptions

--- users/models.py
+++ users/models.py
@@ -1,3 +1,38 @@
+from django.conf import settings
 from django.db import models
+from django.contrib import admin
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Subscription(models.Model):
+    # Define the subscription plan choices
+    STARTER = 'starter'
+    PRO = 'pro'
+    PREMIUM = 'premium'
+    
+    SUBSCRIPTION_CHOICES = [
+        (STARTER, 'Starter'),
+        (PRO, 'Pro'),
+        (PREMIUM, 'Premium'),
+    ]
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    plan = models.CharField(max_length=20, choices=SUBSCRIPTION_CHOICES, default=STARTER)
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username} - {self.get_plan_display()}'
+    
+    def create_subscription(sender, user):
+        Subscription.objects.create(user=user, plan=Subscription.STARTER)
+        
+    def delete_subscription(self, request):
+        subscription = get_object_or_404(Subscription, user=request.user)
+        subscription.delete()
+        return subscription
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'plan', 'start_date', 'end_date')
--- users/templatetags/socialaccount_filters.py
+++ users/templatetags/socialaccount_filters.py
@@ -4,7 +4,8 @@ from django import template
 from allauth.socialaccount.models import SocialAccount
 
 register = template.Library()
-print('loaded')
+
+# required to filter the user's enrolled social connect in profile
 @register.filter
 def is_connected(user, provider_id):
     return SocialAccount.objects.filter(user=user, provider=provider_id).exists()
