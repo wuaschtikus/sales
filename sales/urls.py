980abbe9688fe54ce1f9fcefa67f0564d2ebe8e7
Midori <martin.midori@gmail.com>
2024-08-19 12:15:50 +0200

    tried to fix index site entry on render

--- sales/urls.py
+++ sales/urls.py
@@ -19,7 +19,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+from msgconv.views.index import IndexView
+
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
     path('', include('users.urls')),
     path('', include('msgconv.urls')),
     path('convert/', include('msgconv.urls')),
